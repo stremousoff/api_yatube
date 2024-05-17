8c68fd84b2bf9cb82d8bd561f6117ad6dc0390c7
Anton Stremousov <stremousoff@gmail.com>
2024-05-17 17:23:32 +0300

    v

---
+++ yatube_api/api/mixins.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+
+
+class OnlyAuthorModificationMixin(viewsets.ModelViewSet):
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        instance.delete()
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        serializer.save()
